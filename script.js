65fe15c55a7f1c3084597edc4fd2b3041fdb2dde
alsafi25 <62480618+alsafi25@users.noreply.github.com>
2021-03-01 18:13:27 +0000

    show only one episode

--- script.js
+++ script.js
@@ -18,19 +18,17 @@
 function setup() {
     const allEpisodes = getAllEpisodes();
     makePageForEpisodes(allEpisodes);
-    // EpisodeSelector(allEpisodes)
 }
-
+// Function display all shows and episodes
 function makePageForEpisodes(episodeList) {
     const rootElem = document.getElementById("root");
     const header = document.getElementById('div-header');
     let span = document.createElement('span');
-
     header.appendChild(span);
     let select = document.createElement('select');
     header.appendChild(select);
-
-    span.textContent = `Got ${episodeList.length} episode(s)`;
+    select.add(new Option('Show All', '0'));
+    // span.textContent = `Got ${episodeList.length} episode(s)`;
     let inputText = document.createElement('text');
     for (let i of episodeList) {
         var p = document.createElement('p');
@@ -42,68 +40,71 @@ function makePageForEpisodes(episodeList) {
         var h3 = document.createElement('h3');
         h3.textContent = `${i.name} -  S0${i.season}E0${i.number}`;
         img.textContent = `${i.image.medium}`;
-        p.textContent = `${i.summary}`;
+        p.textContent = `${(i.summary).replace( /(<([^>]+)>)/ig, '')}`;
         select.add(new Option(h3.textContent));
-
-
         // append Element
         rootElem.appendChild(div);
         div.appendChild(h3);
         div.appendChild(img);
         div.appendChild(p);
-
-
-
-
-        select.addEventListener('change', GetSelectedText);
-
-        function GetSelectedText() {
-
-            select.options[select.selectedIndex].text = h3.textContent;
-        }
-
-
-
-
-
-
     }
+    select.addEventListener('change', function() {
+        let input, div, p, i, txtValue, h3;
+        h3 = document.getElementsByTagName('h3');
+        const opt = document.getElementsByTagName('option');
+        const selectedValue = select.options[select.selectedIndex].text;
+        const value = this.selectedIndex;
+        const rootElem = document.getElementById("root");
+        div = rootElem.querySelectorAll("div");
+        let list = Array.from(div);
 
+        const newList = list.filter((item) => {
+                return item.style.display === "";
+            })
+            //  ONLY show the selected episode.  
+        newList.forEach(function(episode, index) {
+            const currentTitle = episode.children[0]
+            if (currentTitle.innerText !== selectedValue) {
+                episode.style.display = 'none';
+            } else if (currentTitle.index === value) {
+                // episode.style.display = "block";
+                makePageForEpisodes();
+            }
+        });
+    });
+
+    /// selected option  lost focus
 }
 
 
 
+// Function to search shows and episodes
 function liveSearchFunction() {
-    let input, filter, div, p, i, txtValue, h3;
-    input = document.getElementById("myInput");
-
-    filter = input.value.toUpperCase();
+    let input, div, p, i, txtValue, h3;
+    const inputText = document.getElementById("myInput");
     const rootElem = document.getElementById("root");
-    div = rootElem.getElementsByTagName("div");
+    const filter = inputText.value.toUpperCase();
+    div = rootElem.querySelectorAll("div");
     const header = document.getElementById('div-header');
-    let span = document.createElement('span');
-    // span.textContent = `Got ${e.length} episode(s)`;
-    header.appendChild(span);
-    // let select = document.createElement('select');
-    // header.appendChild(select);
-    for (i = 0; i < div.length; i++) {
+    let span = document.querySelector('#Search-span')
 
+
+    for (i = 0; i < div.length; i++) {
+        var myRows = div[i].querySelectorAll('.div-block');
         p = div[i].getElementsByTagName("p")[0];
         h3 = div[i].getElementsByTagName("h3")[0];
-
-        txtValue = (h3.textContent || h3.innerText) + (p.textContent || p.innerText);;
-        // span.textContent = `Got ${e} episode(s)`;
+        txtValue = (h3.textContent || h3.innerText) + (p.textContent || p.innerText);
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            div[i].style.display = "";
-
+            div[i].style.display = "block";
         } else {
             div[i].style.display = "none";
         }
-
     }
-
+    let list = Array.from(div);
+    const newList = list.filter((item) => item.style.display === "block");
+    // console.log(newList);
+    span.innerText = `Displaying ${newList.length}/${i} episodes`;
 }
-
 
 
 window.onload = setup;
